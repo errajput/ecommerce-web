61a31a207e726296a514ba86e3c83f286471627c
disha rajput <dishacsevira@gmail.com>
2025-09-09 10:37:46 +0530

    add title and icon null

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,14 @@
 import Header from "@/Components/Header";
 import "./globals.css";
+export const metadata = {
+  title: {
+    default: "E-Commerce Web",
+    template: "%s | My e-commerce web",
+  },
+  icons: {
+    icon: null,
+  },
+};
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
