175ce08a56826e7e4bdd05a7227913d3e835d21f
disha rajput <dishacsevira@gmail.com>
2025-09-10 12:14:33 +0530

    update UI od header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,19 +4,39 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="bg-gray-600 text-white p-4 flex justify-between items-center">
-      <h1 className="text-xl font-bold">Product App</h1>
+    <header className="bg-gradient-to-br from-green-300 to-green-400 text-white p-4 flex justify-between items-center">
+      <h1 className="text-2xl font-bold">Product App</h1>
       <nav className="space-x-9">
-        <Link href="/products" className="cursor-pointer">
+        <Link
+          href="/products"
+          className="cursor-pointer relative transition duration-300 hover:text-green-700 font-bold
+                     after:content-[''] after:absolute after:w-0 after:h-[2px] after:bg-white after:left-0 after:-bottom-1
+                     hover:after:w-full after:transition-all after:duration-300"
+        >
           Products
         </Link>
-        <Link href="/products/add" className="cursor-pointer">
+        <Link
+          href="/products/add"
+          className="cursor-pointer relative transition duration-300 hover:text-green-700 font-bold
+                     after:content-[''] after:absolute after:w-0 after:h-[2px] after:bg-white after:left-0 after:-bottom-1
+                     hover:after:w-full after:transition-all after:duration-300"
+        >
           Add Product
         </Link>
-        <Link href="/register" className="cursor-pointer">
+        <Link
+          href="/register"
+          className="cursor-pointer relative transition duration-300 hover:text-green-700 font-bold
+                     after:content-[''] after:absolute after:w-0 after:h-[2px] after:bg-white after:left-0 after:-bottom-1
+                     hover:after:w-full after:transition-all after:duration-300"
+        >
           Register
         </Link>
-        <Link href="/login" className=" cursor-pointer">
+        <Link
+          href="/login"
+          className="cursor-pointer relative transition duration-300 hover:text-green-700 font-bold
+                     after:content-[''] after:absolute after:w-0 after:h-[2px] after:bg-white after:left-0 after:-bottom-1
+                     hover:after:w-full after:transition-all after:duration-300"
+        >
           Login
         </Link>
       </nav>
