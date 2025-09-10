b0ce053fafe855bf555b2df89b364044d4d0bd3e
disha rajput <dishacsevira@gmail.com>
2025-09-10 17:48:52 +0530

    Add profile

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -39,6 +39,15 @@ export default function Header() {
         >
           Login
         </Link>
+
+        <Link
+          href="/profile"
+          className="cursor-pointer relative transition duration-300 hover:text-green-700 font-bold
+                     after:content-[''] after:absolute after:w-0 after:h-[2px] after:bg-white after:left-0 after:-bottom-1
+                     hover:after:w-full after:transition-all after:duration-300"
+        >
+          Profile
+        </Link>
       </nav>
     </header>
   );
