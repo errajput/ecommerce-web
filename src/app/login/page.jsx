17e88d42a3861d9ecb1bfa5b2754c0f3a206fc7d
disha rajput <dishacsevira@gmail.com>
2025-09-10 15:59:44 +0530

    Add token

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -29,7 +29,10 @@ export default function LoginPage() {
     try {
       const res = await fetch("http://localhost:5000/auth/login", {
         method: "POST",
-        headers: { "Content-Type": "application/json" },
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
         body: JSON.stringify(formData),
       });
 
