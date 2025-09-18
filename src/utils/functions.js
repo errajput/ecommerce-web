6c485bbb97c88e51b1f4642f196d4d7969f92573
disha rajput <dishacsevira@gmail.com>
2025-09-18 11:35:51 +0530

    Add comment for query

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,5 +1,5 @@
 /**
- * To get something TODO:
+ * To get something TODO: It’s a helper to sanitize query(Cleaner URLs,Backend safety,undefine,null and empty string) objects before converting them into URLSearchParams(the query string only contains valid, non-empty parameters).
  */
 export const getQuery = (query) => {
   const checkedQuery = {};
