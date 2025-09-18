e9fbbb9329dc4c2f7f9b1c58318789fc8ac95420
disha rajput <dishacsevira@gmail.com>
2025-09-18 16:52:58 +0530

    use getApi and postApi from http.service file for get order and place order

--- src/services/order.api.js
+++ src/services/order.api.js
@@ -1,28 +1,9 @@
-const API_URL = "http://localhost:5000";
+import { getApi, postApi } from "./http.service";
 
-const ORDER_URL = `${API_URL}/orders`;
-
-export async function getOrders() {
-  const res = await fetch(ORDER_URL, {
-    headers: {
-      Authorization: `Bearer ${localStorage.getItem("token")}`,
-    },
-  });
-
-  if (!res.ok) throw new Error("Failed to fetch orders");
-  return res.json();
-}
+export const getOrders = () => {
+  return getApi("/orders");
+};
 
 export const placeOrder = async () => {
-  const res = await fetch(`${ORDER_URL}/place`, {
-    method: "POST",
-    headers: {
-      Authorization: `Bearer ${getToken()}`,
-    },
-  });
-
-  const data = await res.json();
-  if (!res.ok) throw new Error(data.message);
-
-  return data;
+  return postApi("/orders/place");
 };
